c01f53198bc891d1f9ff63a73e39f249d04ef26a
pauchinpun <a20paumunoli@inspedralbes.cat>
2022-05-05 12:12:46 +0200

    Unir front cuestionarios + back

--- web/vue-projecte-final/src/views/HomeSaludmental.vue
+++ web/vue-projecte-final/src/views/HomeSaludmental.vue
@@ -12,76 +12,30 @@ export default {
       email: "",
       selected: 0,
       motivo: "",
-      estado: [
-        {
-          value: "Alegre",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Alegre.png",
-        },
-        {
-          value: "Desanimado",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Desanimado.png",
-          motivos: {
-            motivo1:
-              "Últimamente estoy siempre cansado sin importar lo que haga",
-            motivo2:
-              "Nada me genera especial interés y me cuesta disfrutar de las cosas",
-            motivo3: "Otro",
-          },
-        },
-        {
-          value: "Irritado",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Irritado.png",
-          motivos: {
-            motivo1:
-              "Estoy pasando situaciones complicadas, estoy a la defensiva",
-            motivo2: "He tenido un mal dia",
-            motivo3: "Otro",
-          },
-        },
-        {
-          value: "Nervioso",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Nervioso.png",
-          motivos: {
-            motivo1: "Estoy viviendo situaciones de incertidumbre",
-            motivo2: "Estoy atravesando adversidades",
-            motivo3: "Otro",
-          },
-        },
-        {
-          value: "Energico",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Enérgico.png",
-        },
-        {
-          value: "Estresado",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Estresado.png",
-          motivos: {
-            motivo1: "Estoy siendo muy autoexigente",
-            motivo2: "Tengo una rutina muy ajetreada, me siento sobrecargado",
-            motivo3: "Otro",
-          },
-        },
-        {
-          value: "Triste",
-          emoji: "@/../public/img/EMOJIS ESTADO ÁNIMO/Triste.png",
-          motivos: {
-            motivo1: "Estoy experimentando un duelo emocional",
-            motivo2: "Estoy viviendo una época de cambios que me asustan",
-            motivo3: "Siento que he fracasado o me da miedo hacerlo",
-            motivo4: "Otro",
-          },
-        },
-      ],
+      estado: null
     };
   },
+
+  beforeMount() {
+    fetch("http://192.168.210.161:9000/salud-mental/estado-emocional").then(response => response.json()).then(
+      data => {
+        this.estado = data.estadoEmocional;
+      }
+    );
+  },
+
+
   methods: {
     onReset(event) {
       event.preventDefault();
       // Reset our form values
       (this.selected = ""), (this.estado = null);
     },
+
     retroceder() {
       window.history.back();
     },
+
     enviarFormulario() {
       var cuestSalud = new URLSearchParams({
         usuario: "ermengol",
@@ -127,16 +81,9 @@ export default {
       </div>
 
       <div class="row justify-content-center">
-        <div
-          class="col-3 d-flex justify-content-center"
-          v-for="(opcion, index) in estado"
-          :key="index"
-          v-bind:value="opcion.value"
-        >
-          <CardVertical
-            @id="this.guardarEstado"
-            :infoCuest="this.estado[index]"
-          />
+        <div class="col-3 d-flex justify-content-center" v-for="(opcion, index) in estado" :key="index"
+          v-bind:value="opcion.value">
+          <CardVertical @id="this.guardarEstado" :infoCuest="this.estado[index]" />
         </div>
 
         <!-- <div class="row">
@@ -157,12 +104,8 @@ export default {
         </div> -->
 
         <div class="col-12 text-center">
-          <input
-            class="btn btn-outline-secondary form-submit"
-            type="button"
-            @click="enviarFormulario()"
-            value="Enviar datos"
-          />
+          <input class="btn btn-outline-secondary form-submit" type="button" @click="enviarFormulario()"
+            value="Enviar datos" />
         </div>
       </div>
     </div>
@@ -176,23 +119,14 @@ export default {
           <div class="card-body">
             <div class="d-grid">
               <div class="botons col-12">
-                <button
-                  v-if="
-                    selected[0].value == 'Alegre' ||
-                    selected[0].value == 'Energico'
-                  "
-                  class="emoji-value btn btn-outline-info"
-                >
+                <button v-if="
+                  selected[0].value == 'Alegre' ||
+                  selected[0].value == 'Energico'
+                " class="emoji-value btn btn-outline-info">
                   Hoy estoy {{ selected[0].value }}
                 </button>
-                <button
-                  class="btn btn-outline-info boton"
-                  type="button"
-                  @click="guardarMotivo($event.target.value)"
-                  :key="index"
-                  v-for="(estado, index) in selected[0].motivos"
-                  :value="estado"
-                >
+                <button class="btn btn-outline-info boton" type="button" @click="guardarMotivo($event.target.value)"
+                  :key="index" v-for="(estado, index) in selected[0].motivos" :value="estado">
                   {{ estado }}
                 </button>
               </div>
@@ -203,11 +137,7 @@ export default {
     </div>
 
     <div id="retroceder" class="text-center mt-5">
-      <button
-        @click="retroceder()"
-        type="button"
-        class="btn btn-outline-danger"
-      >
+      <button @click="retroceder()" type="button" class="btn btn-outline-danger">
         Retroceder
       </button>
     </div>
@@ -219,35 +149,43 @@ select {
   background-color: white;
   color: gray;
 }
+
 .cuestionario_estado {
   background-color: rgb(199, 234, 255);
   margin-top: 50px;
   padding: 30px;
 }
+
 #card-horizontal {
   margin-top: 2%;
   margin-left: auto;
   margin-right: auto;
   max-width: 50%;
 }
+
 #emoji-card {
   font-size: 800%;
 }
+
 .img-emoji {
   text-align: center;
 }
+
 .emoji-value {
   font-size: 350%;
 }
+
 form {
   padding-top: 5%;
   text-align: center;
   margin-left: 35%;
   margin-right: 35%;
 }
+
 .form-input-email {
   width: 250px;
 }
+
 .btn {
   margin: 20px;
 }
