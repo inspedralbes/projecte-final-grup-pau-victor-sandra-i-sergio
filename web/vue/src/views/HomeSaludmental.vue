da0f3bd044cfed087f4f8ff75d561dbfbdb71390
pauchinpun <a20paumunoli@inspedralbes.cat>
2022-05-09 08:20:48 +0200

    cambio ip fetch

--- web/vue/src/views/HomeSaludmental.vue
+++ web/vue/src/views/HomeSaludmental.vue
@@ -19,8 +19,8 @@ export default {
   },
 
   beforeMount() {
-    // fetch("http://192.168.210.161:9000/salud-mental/estado-emocional")
-    fetch("http://localhost:9000/salud-mental/estado-emocional")
+    fetch("http://192.168.210.161:9000/salud-mental/estado-emocional")
+    // fetch("http://localhost:9000/salud-mental/estado-emocional")
       .then((response) => response.json())
       .then((data) => {
         this.estado = data.estadoEmocional;
@@ -46,8 +46,8 @@ export default {
         motivo: this.motivo,
       });
       console.log(cuestSalud);
-      // fetch("http://192.168.210.161:9000/salud-mental/respuesta-cuestionario", {
-      fetch("http://localhost:wd/salud-mental/respuesta-cuestionario", {
+      ("http://192.168.210.161:9000/salud-mental/respuesta-cuestionario", {
+      // fetch("http://localhost:wd/salud-mental/respuesta-cuestionario", {
         method: "POST",
         body: cuestSalud,
       })
