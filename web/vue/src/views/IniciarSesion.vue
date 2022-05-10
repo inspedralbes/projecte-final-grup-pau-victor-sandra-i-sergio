093195e77c2bcee0e37d5563ecb31ca7e1121a2e
pauchinpun <a20paumunoli@inspedralbes.cat>
2022-05-10 14:27:17 +0200

    login responsive

--- web/vue/src/views/IniciarSesion.vue
+++ web/vue/src/views/IniciarSesion.vue
@@ -19,45 +19,25 @@ export default {
 <template>
   <div class="container align-items-center justify-content-center text-center">
     <div class="flip-box">
-      <div
-        class="flip-box-inner"
-        v-bind:class="[this.click_registro ? 'flip' : '']"
-      >
+      <div class="flip-box-inner" v-bind:class="[this.click_registro ? 'flip' : '']">
         <!-- INICIAR SESIÓN -->
         <div class="flip-box-front carta_singup_front row row-cols shadow">
           <div class="col-12 iniciar_sesion">
             <h3>Iniciar Sesión</h3>
             <div class="input-group mb-2 mt-5">
-              <span class="input-group-text material-symbols-outlined"
-                >alternate_email</span
-              >
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Correo"
-                aria-label="Correo"
-                aria-describedby="basic-addon1"
-              />
+              <span class="input-group-text material-symbols-outlined">alternate_email</span>
+              <input type="text" class="form-control" placeholder="Correo" aria-label="Correo"
+                aria-describedby="basic-addon1" />
             </div>
 
             <div class="input-group mb-2 mt-3">
-              <span class="input-group-text material-symbols-outlined"
-                >lock</span
-              >
-              <input
-                type="password"
-                class="form-control"
-                placeholder="Contraseña"
-                aria-label="Contraseña"
-                aria-describedby="basic-addon2"
-              />
+              <span class="input-group-text material-symbols-outlined">lock</span>
+              <input type="password" class="form-control" placeholder="Contraseña" aria-label="Contraseña"
+                aria-describedby="basic-addon2" />
             </div>
 
             <button class="btn btn-primary mt-4">Iniciar Sesión</button>
-            <div
-              class="login_singup col-12 mt-4 mb-5"
-              @click="cambiar_registro"
-            >
+            <div class="login_singup col-12 mt-4 mb-5" @click="cambiar_registro">
               <span>No tienes cuenta?</span>
             </div>
           </div>
@@ -68,42 +48,21 @@ export default {
             <h3>Unete a Genki Body</h3>
 
             <div class="input-group mb-2 mt-5">
-              <span class="input-group-text material-symbols-outlined"
-                >person</span
-              >
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Nombre"
-                aria-label="Nombre"
-                aria-describedby="basic-addon1"
-              />
+              <span class="input-group-text material-symbols-outlined">person</span>
+              <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre"
+                aria-describedby="basic-addon1" />
             </div>
 
             <div class="input-group mb-2 mt-3">
-              <span class="input-group-text material-symbols-outlined"
-                >alternate_email</span
-              >
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Correo"
-                aria-label="Correo"
-                aria-describedby="basic-addon1"
-              />
+              <span class="input-group-text material-symbols-outlined">alternate_email</span>
+              <input type="text" class="form-control" placeholder="Correo" aria-label="Correo"
+                aria-describedby="basic-addon1" />
             </div>
 
             <div class="input-group mb-2 mt-3">
-              <span class="input-group-text material-symbols-outlined"
-                >lock</span
-              >
-              <input
-                type="password"
-                class="form-control"
-                placeholder="Contraseña"
-                aria-label="Contraseña"
-                aria-describedby="basic-addon2"
-              />
+              <span class="input-group-text material-symbols-outlined">lock</span>
+              <input type="password" class="form-control" placeholder="Contraseña" aria-label="Contraseña"
+                aria-describedby="basic-addon2" />
             </div>
 
             <button class="btn btn-primary mt-4">Registrar</button>
@@ -156,7 +115,7 @@ h3 {
 
 /***** Transformación Flip *****/
 
-.flip-box {
+/* .flip-box {
   background-color: transparent;
   width: 380px;
   height: 450px;
@@ -180,6 +139,34 @@ h3 {
   position: absolute;
   width: 380px;
   height: 450px;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+} */
+
+.flip-box {
+  background-color: transparent;
+  width: 40vw;
+  height: auto;
+  perspective: 10000px;
+}
+
+.flip-box-inner {
+  width: 40vw;
+  height: 50vh;
+  text-align: center;
+  transition: transform 1.8s;
+  transform-style: preserve-3d;
+}
+
+.flip {
+  transform: rotateY(180deg);
+}
+
+.flip-box-front,
+.flip-box-back {
+  position: absolute;
+  width: 40vh;
+  height: auto;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 }
