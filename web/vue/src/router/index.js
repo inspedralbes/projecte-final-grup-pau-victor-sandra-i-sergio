c10c9addf672de49eae5b7ae3011178d942eceb3
pauchinpun <a20paumunoli@inspedralbes.cat>
2022-05-16 14:26:07 +0200

    cambios

--- web/vue/src/router/index.js
+++ web/vue/src/router/index.js
@@ -6,63 +6,58 @@ import HomeSaludmental from "../views/HomeSaludmental.vue";
 import HomeInicio from "../views/HomeInicio.vue";
 import HomeDescanso from "../views/HomeDescanso.vue";
 import IniciarSesion from "../views/IniciarSesion.vue";
-import Articulos from "../views/Articulos.vue";
-<<<<<<< Updated upstream
+import Articulos from "../views/Articulos.vue"; 
 import EditarUsuario from "../views/EditarUsuario.vue";
-=======
-import RegistroDatos from "../views/RegistroDatos.vue";
->>>>>>> Stashed changes
+import RegistroDatos from "../views/RegistroDatos.vue"; 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeInicio,
-    },
-    {
-      path: "/ejercicio",
-      name: "ejercicio",
-      component: HomeEjercicio,
-    },
-    {
-      path: "/alimentacion",
-      name: "alimentacion",
-      component: HomeAlimentacion,
-    },
-    {
-      path: "/saludmental",
-      name: "saludmental",
-      component: HomeSaludmental,
-    },
-    {
-      path: "/descanso",
-      name: "descanso",
-      component: HomeDescanso,
-    },
-    {
-      path: "/iniciarSesion",
-      name: "iniciarSesion",
-      component: IniciarSesion,
-    },
-    {
-      path: "/articulos",
-      name: "articulos",
-      component: Articulos,
-    },
-    {
-<<<<<<< Updated upstream
-      path: "/editarUsuario",
-      name: "editarUsuario",
-      component: EditarUsuario,
-=======
-      path: "/registroDatos",
-      name: "registroDatos",
-      component: RegistroDatos,
->>>>>>> Stashed changes
-    },
-  ],
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes: [{
+            path: "/",
+            name: "home",
+            component: HomeInicio,
+        },
+        {
+            path: "/ejercicio",
+            name: "ejercicio",
+            component: HomeEjercicio,
+        },
+        {
+            path: "/alimentacion",
+            name: "alimentacion",
+            component: HomeAlimentacion,
+        },
+        {
+            path: "/saludmental",
+            name: "saludmental",
+            component: HomeSaludmental,
+        },
+        {
+            path: "/descanso",
+            name: "descanso",
+            component: HomeDescanso,
+        },
+        {
+            path: "/iniciarSesion",
+            name: "iniciarSesion",
+            component: IniciarSesion,
+        },
+        {
+            path: "/articulos",
+            name: "articulos",
+            component: Articulos,
+        },
+        {
+            path: "/editarUsuario",
+            name: "editarUsuario",
+            component: EditarUsuario,
+        }, {
+            path: "/registroDatos",
+            name: "registroDatos",
+            component: RegistroDatos,
+        },
+    ],
 });
 
 export default router;
