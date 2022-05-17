32ac5c317b9bbfa56ccc5edb25619ba0a0ddc2b4
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2022-05-17 13:26:05 +0200

    formulario editar usuario

--- web/vue/src/views/EditarUsuario.vue
+++ web/vue/src/views/EditarUsuario.vue
@@ -7,18 +7,67 @@ export default {
     return {
       nombreUsuario: null,
       emailUsuario: null,
+      datosUsuario: null,
       ctx: null,
       myChart: null,
+      ocupacion: null,
+      tiempo: null,
+      nivel: null,
     };
   },
   computed: {
     ...mapStores(sesionStore),
   },
-  mounted() {
+
+  created() {
     this.nombreUsuario = this.sesionStore.getUsuario.nombreApellidos;
-    console.log(this.nombreUsuario);
     this.emailUsuario = this.sesionStore.getUsuario.email;
-    console.log(this.emailUsuario);
+    this.datosUsuario = this.sesionStore.getUsuario.datosPersonales;
+  },
+
+  mounted() {
+    document.querySelectorAll("#nivel option").forEach((opt) => {
+      if (opt.value == this.datosUsuario.nivelFisico) {
+        this.nivel = this.datosUsuario.nivelFisico;
+        opt.setAttribute("selected", "");
+      }
+    });
+    document.querySelectorAll("#ocupacion option").forEach((opt) => {
+      if (opt.value == this.datosUsuario.ocupacion) {
+        this.ocupacion = this.datosUsuario.ocupacion;
+        opt.setAttribute("selected", "");
+      }
+    });
+    document.querySelectorAll("#tiempo option").forEach((opt) => {
+      if (opt.value == this.datosUsuario.disponibilidadTiempo) {
+        this.tiempo = this.datosUsuario.disponibilidadTiempo;
+        opt.setAttribute("selected", "");
+      }
+    });
+  },
+  methods: {
+    actualizarDatos() {
+      let datos = new FormData();
+      datos.append(
+        "nombreApellidos",
+        document.getElementById("floatingnombre").value
+      );
+      datos.append("edad", document.getElementById("floatingEdad").value);
+      datos.append("ocupacion", this.ocupacion);
+      datos.append("tiempo", this.tiempo);
+      datos.append("nivel", this.nivel);
+      for (var pair of datos.entries()) {
+        console.log(pair[0] + ", " + pair[1]);
+      }
+      fetch("http://192.168.210.162:9000/", {
+        method: "POST",
+        body: datos,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    },
   },
 };
 </script>
@@ -36,77 +85,94 @@ export default {
           <input
             type="text"
             class="form-control"
-            id="floatingInput"
+            id="floatingnombre"
             :value="this.nombreUsuario"
             placeholder="nombre"
           />
           <label for="floatingInput">Nombre</label>
         </div>
-      </div>
-      <div class="col-12 col-md-8">
         <div class="form-floating mb-3">
           <input
             type="email"
             class="form-control"
-            id="floatingInput"
+            id="floatingemail"
             :value="this.emailUsuario"
             placeholder="name@example.com"
+            disabled
           />
           <label for="floatingInput">Email address</label>
         </div>
+      </div>
+      <div class="col-12 col-md-8">
         <div class="row">
           <div class="col-12 col-md-6">
-            <div class="form-floating">
+            <div class="form-floating mb-3">
               <input
                 type="number"
                 class="form-control"
                 id="floatingEdad"
-                value="20"
+                :value="this.datosUsuario.edad"
                 placeholder="Edad"
               />
               <label for="floatingEdad">Edad</label>
             </div>
-            <div class="form-floating">
+            <div class="form-floating mb-3">
               <input
                 type="text"
                 class="form-control"
                 id="floatingSexo"
-                value="sexo"
+                :value="this.datosUsuario.sexo"
                 placeholder="Sexo"
+                disabled
               />
               <label for="floatingSexo">Sexo</label>
             </div>
-            <div class="form-floating">
-              <input
-                type="text"
-                class="form-control"
-                id="floatingOcupacion"
-                value="Estudiando"
-                placeholder="Ocupacion"
-              />
+            <div class="form-floating mb-3">
+              <select
+                id="ocupacion"
+                class="form-select"
+                aria-label="Default select example"
+                v-model="this.ocupacion"
+              >
+                <option value="Trabajo">Trabajo</option>
+                <option value="Estudio">Estudio</option>
+                <option value="Otro">Otro</option>
+              </select>
               <label for="floatingOcupacion">Ocupacion</label>
             </div>
           </div>
           <div class="col-12 col-md-6">
-            <div class="form-floating">
-              <input
-                type="text"
-                class="form-control"
-                id="floatingTiempo"
-                value="+1"
-                placeholder="Tiempo"
-              />
+            <div class="form-floating mb-3">
+              <select
+                id="tiempo"
+                class="form-select"
+                aria-label="Default select example"
+                v-model="this.tiempo"
+              >
+                <option value="~ 15 min">~ 15 min</option>
+                <option value="30 min">30 min</option>
+                <option value="45 min">45 min</option>
+                <option value="+1 h">+1 h</option>
+              </select>
               <label for="floatingEdad">Tiempo</label>
             </div>
-            <div class="form-floating">
-              <input
-                type="text"
-                class="form-control"
-                id="floatingNivel"
-                value="Intermedio"
-                placeholder="Nivel"
-              />
+            <div class="form-floating mb-3">
+              <select
+                id="nivel"
+                class="form-select"
+                aria-label="Default select example"
+                v-model="this.nivel"
+              >
+                <option value="Principiante">Principiante</option>
+                <option value="Intermedio">Intermedio</option>
+                <option value="Avanzado">Avanzado</option>
+              </select>
               <label for="floatingEdad">Nivel</label>
+            </div>
+            <div class="form-floating">
+              <button @click="actualizarDatos()" class="btn btn-success">
+                Guardar
+              </button>
             </div>
           </div>
         </div>
